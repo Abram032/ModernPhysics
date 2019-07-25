67ed250077d2b29db22f8812b9f2ee08dd0ee682
Adam Błaszczyk <adam.blaszczyk@outlook.in>
2019-07-25 15:57:01 +0200

    Implemented index page, small fixes for resource page

--- src/ModernPhysics.Web/Pages/Index.cshtml.cs
+++ src/ModernPhysics.Web/Pages/Index.cshtml.cs
@@ -4,14 +4,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ModernPhysics.Web.Data;
 
 namespace ModernPhysics.Web.Pages
 {
     public class IndexModel : PageModel
     {
+        private WebAppDbContext _context;
+
+        public IndexModel(WebAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Models.Page> Posts { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string Search { get; set; }
+
         public void OnGet()
         {
-
+            Posts = _context.Pages.Include(p => p.Category)
+                    .Include(p => p.PageTags)
+                    .ThenInclude(p => p.Tag)
+                    .Take(2);
         }
     }
 }
